a5fe000d232269f7c09c07d17d990bc32d98bf18
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2026-04-03 16:58:04 -0300

    feat: creating and using custom tools

---
+++ 1.1.weather_researcher.py
@@ -0,0 +1,29 @@
+from agno.agent import Agent
+from agno.tools.tavily import TavilyTools
+from agno.models.groq import Groq
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def celsius_to_fh(temperature_celsius: float):
+    """"
+    Converts temperature from Celsius to Fahrenheit.
+
+    Args:
+        temperature_celsius (float): Temperature in degrees Celsius
+
+    Returns:
+        float: Temperature in Fahrenheit
+    """
+    return (temperature_celsius * 9/5) + 32
+
+
+
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[TavilyTools(),
+           celsius_to_fh],
+    debug_mode=True
+)
+
+agent.print_response("Use your tools to search the temperature today in São Paulo city in Fahrenheit.")
